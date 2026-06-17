22c9d272eb4b9e19bbb59c3dd6cf2e37fad9ebf7
Artem <artvorozhbyt@gmail.com>
2026-06-17 20:32:09 +0200

    Initial clean architecture setup

---
+++ docs/NutritionTracker.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace NutritionTracker.Infrastructure;
+
+public class Class1
+{
+
+}
---
+++ tests/NutritionTracker.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace NutritionTracker.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
